01cc21b953483e149bde1309576bcc6423bbfdf2
Data-Nexus <derek.m.meyer@gmail.com>
2022-04-03 07:44:10 -0400

    fork init

---
+++ src/eip721/index.ts
@@ -0,0 +1,43 @@
+import {
+	Account,
+	Transfer,
+} from '../../generated/schema'
+
+import {
+	Transfer       as TransferEvent,
+} from '../../generated/IERC721/IERC721'
+
+import {
+	fetchRegistry,
+	fetchToken,
+} from '../utils/erc721'
+
+import {
+	events,
+	transactions,
+} from '../../src/graphprotocol-utils'
+
+export function handleTransfer(event: TransferEvent): void {
+	let registry = fetchRegistry(event.address)
+	if (registry != null)
+	{
+		let token = fetchToken(registry, event.params.tokenId)
+		let from  = new Account(event.params.from.toHex())
+		let to    = new Account(event.params.to.toHex())
+
+		token.owner = to.id
+
+		registry.save()
+		token.save()
+		from.save()
+		to.save()
+
+		let ev = new Transfer(events.id(event))
+		ev.transaction = transactions.log(event).id
+		ev.timestamp   = event.block.timestamp
+		ev.token       = token.id
+		ev.from        = from.id
+		ev.to          = to.id
+		ev.save()
+	}
+}
---
+++ src/graphprotocol-utils.ts
@@ -0,0 +1,35 @@
+import {
+	ethereum,
+} from '../node_modules/@graphprotocol/graph-ts'
+
+export namespace events {
+	export function id(event: ethereum.Event): string {
+		return event.block.number.toString().concat('-').concat(event.logIndex.toString())
+	}
+}
+
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+
+export namespace constants {
+	export let   BIGINT_ZERO      = BigInt.fromI32(0)
+	export let   BIGINT_ONE       = BigInt.fromI32(1)
+	export let   BIGDECIMAL_ZERO  = new BigDecimal(constants.BIGINT_ZERO)
+	export let   BIGDECIMAL_ONE   = new BigDecimal(constants.BIGINT_ONE)
+	export const ADDRESS_ZERO     = '0x0000000000000000000000000000000000000000'
+	export const BYTES32_ZERO     = '0x0000000000000000000000000000000000000000000000000000000000000000'
+}
+
+import {
+	Transaction,
+} from '../generated/schema'
+
+export namespace transactions {
+	export function log(event: ethereum.Event): Transaction {
+		let tx = new Transaction(event.transaction.hash.toHex())
+		tx.timestamp   = event.block.timestamp
+		tx.blockNumber = event.block.number
+		tx.save()
+		return tx as Transaction
+	}
+	export type Tx = Transaction
+}
